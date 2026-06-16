f7bf6b2df6793c732037b51a451347ed6b03e899
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-16 20:58:05 +0530

    Update App.jsx

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <main className="h-screen w-full bg-gray-950">
-      
+      <aside className="h-full w-1/4 bg-amber-50">
+
+      </aside>
+      <section className="w-3/4"></section>
     </main>
   )
 }
